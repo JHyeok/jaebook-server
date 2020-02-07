a38855525c595db44d6fdbff1b2e4296cd23a3db
JHyeok <dev.jhyeok@gmail.com>
2020-02-07 23:16:49 +0900

    feat: User 컨트롤러, 서비스를 이용한 API 구현 User의 생성, 조회, 삭제, 업데이트 기능 구현 User의 Password를 암호화해서 저장

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,33 @@
 import { UserService } from "../services/UserService";
-import { JsonController, Get } from "routing-controllers";
+import { JsonController, Get, Param, Body, Post, Put, Delete } from "routing-controllers";
 import { User } from "../entities/User";
 
 @JsonController("/users")
 export class UserController {
     constructor(private userService: UserService) {}
 
-    @Get()
-    public find(): Promise<User[]> {
-        return this.userService.getUsers();
+    @Post()
+    public create(@Body() user: User): Promise<User> {
+        return this.userService.createUser(user);
+    }
+
+    @Get("")
+    public getAll(): Promise<User[]> {
+        return this.userService.getAllUsers();
+    }
+
+    @Get("/:id")
+    public getOne(@Param("id") id: string): Promise<User> {
+        return this.userService.getUsersById(id);
+    }
+
+    @Put("/:id")
+    public update(@Param("id") id: string, @Body() user: User): Promise<User> {
+        return this.userService.updateUser(id, user);
+    }
+
+    @Delete("/:id")
+    public delete(@Param("id") id: string): Promise<string> {
+        return this.userService.deleteUser(id);
     }
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,13 +1,35 @@
 import { Service } from "typedi";
-import { OrmRepository } from "typeorm-typedi-extensions";
+import { InjectRepository } from "typeorm-typedi-extensions";
 import { User } from "../entities/User";
 import { UserRepository } from "../repositories/UserRepository";
 
 @Service()
 export class UserService {
-    constructor(@OrmRepository() private userRepository: UserRepository) {}
+    constructor(@InjectRepository() private userRepository: UserRepository) {}
 
-    public getUsers(): Promise<User[]> {
+    public createUser(user: User): Promise<User> {
+        user.hashPassword();
+        const newUser = this.userRepository.save(user);
+        return newUser;
+    }
+
+    public getAllUsers(): Promise<User[]> {
         return this.userRepository.find();
+    }
+
+    public getUsersById(id: string): Promise<User> {
+        return this.userRepository.findOne(id);
+    }
+
+    public updateUser(id: string, user: User): Promise<User> {
+        user.hashPassword();
+        user.id = id;
+        return this.userRepository.save(user);
+    }
+
+    public async deleteUser(id: string): Promise<string> {
+        const userToRemove = await this.userRepository.findOne(id);
+        this.userRepository.remove(userToRemove);
+        return `remove user: ${id}`;
     }
 }
