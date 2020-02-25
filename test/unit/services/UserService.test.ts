046da70fb5e85dbfc5831c45f93ad68c85f172aa
JHyeok <dev.jhyeok@gmail.com>
2020-02-25 21:12:45 +0900

    test: UserService 테스트 수정

--- test/unit/services/UserService.test.ts
+++ test/unit/services/UserService.test.ts
@@ -4,10 +4,10 @@ import { UserService } from "../../../src/services/UserService";
 import { UserRepository } from "../../../src/repositories/UserRepository";
 import { User } from "../../../src/entities/User";
 
-describe("User Service", () => {
+describe("UserService", () => {
     let db: Connection;
-    let userService: UserService;
     let userRepository: UserRepository;
+    let userService: UserService;
 
     beforeAll(async () => {
         db = await createMemoryDatabase([User]);
@@ -25,27 +25,26 @@ describe("User Service", () => {
 
     let newUserId: string;
 
-    it("유저를 생성하고 정보를 반환한다", async () => {
+    it("유저를 생성하고 생성된 정보를 반환한다", async () => {
         const newUser = await userService.createUser(userRequest as any);
         newUserId = newUser.id;
-
         expect(newUser.realName).toBe(userRequest.realName);
         expect(newUser.email).toBe(userRequest.email);
     });
 
-    it("Id가 일치하는 유저를 찾아서 반환한다", async () => {
-        const newUserInDB = await userService.getUsersById(newUserId);
-
-        expect(newUserInDB.realName).toBe(userRequest.realName);
-        expect(newUserInDB.email).toBe(userRequest.email);
+    it("Id가 일치하는 유저를 찾아서 유저 정보를 반환한다", async () => {
+        const user = await userService.getUsersById(newUserId);
+        expect(user).toBeInstanceOf(User);
+        expect(user.realName).toBe(userRequest.realName);
+        expect(user.email).toBe(userRequest.email);
     });
 
-    it("이메일이 사용 중인지, 사용하지 않는지 체크해서 반환한다", async () => {
-        // 유저 테이블에 이미 등록되어서 사용중인 이메일
+    it("이미 등록된 이메일의 중복 여부를 검사하면 true를 반환한다", async () => {
         const isDuplicateUser = await userService.isDuplicateUser(userRequest.email);
         expect(isDuplicateUser).toBeTruthy();
+    });
 
-        // 유저 테이블에 등록되지 않는 이메일
+    it("등록되지 않은 이메일의 중복 여부를 검사하면 false를 반환한다", async () => {
         const isUnDuplicateUser = await userService.isDuplicateUser("null@gmail.com");
         expect(isUnDuplicateUser).toBeFalsy();
     });
