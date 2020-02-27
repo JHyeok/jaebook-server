4bad42081a382a57f4259650ccbe1bb18d0ab790
JHyeok <dev.jhyeok@gmail.com>
2020-02-27 18:15:37 +0900

    test: Controller 단위 테스트 추가

---
+++ test/unit/controllers/AuthController.test.ts
@@ -0,0 +1,63 @@
+import { Connection } from "typeorm";
+import { createMemoryDatabase } from "../../utils/CreateMemoryDatabase";
+import { AuthController } from "../../../src/controllers/AuthController";
+import { UserService } from "../../../src/services/UserService";
+import { AuthService } from "../../../src/services/AuthService";
+import { UserRepository } from "../../../src/repositories/UserRepository";
+import { User } from "../../../src/entities/User";
+import { UserSeed } from "../seeds/UserTestSeed";
+
+describe("AuthController", () => {
+    let db: Connection;
+    let userRepository: UserRepository;
+    let userService: UserService;
+    let authService: AuthService;
+    let authController: AuthController;
+
+    beforeAll(async () => {
+        db = await createMemoryDatabase([User]);
+        userRepository = db.getRepository(User);
+        userService = new UserService(userRepository);
+        authService = new AuthService(userRepository);
+        authController = new AuthController(authService, userService);
+        await userRepository.save(UserSeed);
+    });
+
+    afterAll(() => db.close());
+
+    const userRequest = {
+        realName: "홍길동2",
+        email: "hellojest2@gmail.com",
+        password: "password",
+    };
+
+    it("POST /api/auth/register 를 성공한다", async () => {
+        const result = await authController.register(userRequest as any);
+        expect(result.userInfo.email).toBe(userRequest.email);
+        expect(result.userInfo.realName).toBe(userRequest.realName);
+    });
+
+    it("중복된 이메일로 POST /api/auth/register 에서 오류를 반환한다", async () => {
+        const result = await authController.register(userRequest as any);
+        expect(result.code).toBe("ERR_REGISTER_DUPLICATE_EMAIL");
+        expect(result.message).toBe("이미 사용 중인 이메일입니다.");
+    });
+
+    it("POST /api/auth/login 을 성공한다", async () => {
+        const result = await authController.login("hellojest@gmail.com", "password");
+        expect(result.userInfo.email).toBe("hellojest@gmail.com");
+        expect(result.userInfo.realName).toBe("홍길동");
+    });
+
+    it("틀린 이메일로 POST /api/auth/login 에서 오류를 반환한다", async () => {
+        const result = await authController.login("null@gmail.com", "password");
+        expect(result.code).toBe("ERR_LOGIN_EMAIL_PASSWORD");
+        expect(result.message).toBe("유효하지 않은 사용자 이메일/비밀번호 입니다.");
+    });
+
+    it("틀린 비밀번호로 POST /api/auth/login 에서 오류를 반환한다", async () => {
+        const result = await authController.login("hellojest@gmail.com", "notpassword");
+        expect(result.code).toBe("ERR_LOGIN_EMAIL_PASSWORD");
+        expect(result.message).toBe("유효하지 않은 사용자 이메일/비밀번호 입니다.");
+    });
+});
---
+++ test/unit/controllers/UserController.test.ts
@@ -0,0 +1,51 @@
+import { Connection } from "typeorm";
+import { createMemoryDatabase } from "../../utils/CreateMemoryDatabase";
+import { UserController } from "../../../src/controllers/UserController";
+import { UserService } from "../../../src/services/UserService";
+import { UserRepository } from "../../../src/repositories/UserRepository";
+import { User } from "../../../src/entities/User";
+import { UserSeed } from "../seeds/UserTestSeed";
+
+describe("UserController", () => {
+    let db: Connection;
+    let userRepository: UserRepository;
+    let userService: UserService;
+    let userController: UserController;
+
+    beforeAll(async () => {
+        db = await createMemoryDatabase([User]);
+        userRepository = db.getRepository(User);
+        userService = new UserService(userRepository);
+        userController = new UserController(userService);
+        await userRepository.save(UserSeed);
+    });
+
+    afterAll(() => db.close());
+
+    const userRequest = {
+        realName: "홍길동",
+        email: "hellojest@gmail.com",
+        password: "password",
+    };
+
+    let testUserId: string;
+
+    it("GET /api/users 를 성공한다", async () => {
+        const result = await userController.getAll();
+        expect(result[0].email).toBe(UserSeed[0].email);
+        expect(result[0].realName).toBe(UserSeed[0].realName);
+    });
+
+    it("POST /api/users 를 성공한다", async () => {
+        const result = await userController.create(userRequest as any);
+        testUserId = result.id;
+        expect(result.email).toBe(userRequest.email);
+        expect(result.realName).toBe(userRequest.realName);
+    });
+
+    it("GET /api/users/:id 를 성공한다", async () => {
+        const result = await userController.getOne(testUserId);
+        expect(result.email).toBe(userRequest.email);
+        expect(result.realName).toBe(userRequest.realName);
+    });
+});
