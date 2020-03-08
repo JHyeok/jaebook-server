14dc669940620881c6fcf56bfeffd6e171b1c752
JHyeok <dev.jhyeok@gmail.com>
2020-03-08 12:04:52 +0900

    fix: e2e 테스트 - #1

--- test/e2e/api/users.test.ts
+++ test/e2e/api/users.test.ts
@@ -1,25 +1,37 @@
-import request from "supertest";
+import "reflect-metadata";
+import { Connection } from "typeorm";
+import { createMemoryDatabase } from "../../utils/CreateMemoryDatabase";
+import { agent, Response } from "supertest";
 import app from "../utils/testApp";
-
-/*
-beforeAll(async () => {
-});
-
-afterAll(() => {
-});
-*/
+import { UserRepository } from "../../../src/repositories/UserRepository";
+import { UserSeed } from "../../unit/seeds/UserTestSeed";
 
 const setHeader = (): { Accept: string } => ({
     Accept: "application/json",
 });
 
-// Example
 describe("GET /api/users", () => {
-    it("404: 유저 정보를 요청하고 데이터가 반환된다", async () => {
-        const response = await request(app)
-            .get("/api/users")
-            .set(setHeader());
+    let db: Connection;
+    let userRepository: UserRepository;
 
-        expect(response.status).toBe(404);
+    beforeAll(async done => {
+        db = await createMemoryDatabase();
+        userRepository = db.getCustomRepository(UserRepository);
+        await userRepository.save(UserSeed);
+        setTimeout(done, 1000);
+    });
+
+    afterAll(() => db.close());
+
+    it("200: 유저 정보를 요청하고 데이터가 반환된다", done => {
+        agent(app)
+            .get("/api/users")
+            .set(setHeader())
+            .expect(200)
+            .end((err: any, res: Response) => {
+                const { body } = res;
+                expect(body[0].realName).toEqual("홍길동");
+                done();
+            });
     });
 });
